5606e5b6890d7d8bd052d3952a1ef0a98c243199
Prince <princecultura10@gmail.com>
2025-08-31 14:52:33 +0800

    Create USERINPUT.java

---
+++ USERINPUT.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+       
+     int age = 0;
+     age += 1;
+     
+     System.out.println("What's your age: ");
+     Scanner scanner = new Scanner(System.in);
+     age = scanner.nextInt();
+     scanner.close();
+     
+     System.out.println("My age is: " + age);
+        
+    }
+}
